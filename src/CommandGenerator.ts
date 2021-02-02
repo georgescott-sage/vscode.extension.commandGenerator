46e592ab7a822d9f0ccde8e1710e244b0c3b0f50
george.scott <george.scott@sage.com>
2021-02-02 14:11:29 +0000

    add todos for command generation

--- src/CommandGenerator.ts
+++ src/CommandGenerator.ts
@@ -17,6 +17,11 @@ export class CommandGenerator { //implements IDisposable {
       commandName = commandName.endsWith('Command') ? commandName : commandName += 'Command';
     }
 
+    //TODO: create files from template
+    //TODO: parse the servoce name from the workspace?
+    //TODO: work out where to place the files in the current workspace
+    //TODO: add files to workspace 
+
     vscode.window.showInformationMessage(`Command: '${commandName}' successfully created`);
   }
 
