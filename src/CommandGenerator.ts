d23b3d3cf7756d6aaa853291e28f4000516a73ef
george.scott <george.scott@sage.com>
2021-02-12 11:18:17 +0000

    add function for file uri creation

--- src/CommandGenerator.ts
+++ src/CommandGenerator.ts
@@ -12,23 +12,20 @@ export class CommandGenerator { //implements IDisposable {
     let workspaceDetail : Workspace | undefined = await parser.getProjectWorkspaceDetail();
 		if(!workspaceDetail || workspaceDetail == undefined) {
 			window.showInformationMessage(`Please open an SBC workspace at the route folder`);
+      return;
     }
     
     let commandName : string | undefined = await this.showInputBox();
 
     if (!commandName) {
+      window.showInformationMessage(`Please enter a name for the command`);
       return;
     }
-    else {
-      commandName = commandName.endsWith('Command') ? commandName : commandName += 'Command';
-    }
+    commandName = commandName.endsWith('Command') ? commandName : commandName += 'Command';
 
-    const filename = `${commandName}${this.extension}`;
-    const commandFolderPath = `/src/${workspaceDetail?.name}.Domain.Core/UseCases`
-    const folderPath = path.join(workspaceDetail?.root.fsPath ?? '/', commandFolderPath);
-    const fullpath = path.join(folderPath, filename);
+    const filePath = this.getPath(commandName, workspaceDetail);
     let workspaceEdit = new WorkspaceEdit();
-    workspaceEdit.createFile(vscode.Uri.file(fullpath));
+    workspaceEdit.createFile(filePath);
     vscode.workspace.applyEdit(workspaceEdit);
 
     window.showInformationMessage(`Command: '${commandName}' successfully created`);
@@ -42,6 +39,13 @@ export class CommandGenerator { //implements IDisposable {
       validateInput: this.validate,
     });
     return result;
+  }
+
+  getPath(commandName: string, workspace: Workspace | undefined): vscode.Uri {
+    const filename = `${commandName}${this.extension}`;
+    const commandFolderPath = `/src/${workspace?.name}.Domain.Core/UseCases`
+    const folderPath = path.join(workspace?.root.fsPath ?? '/', commandFolderPath);
+    return vscode.Uri.file(path.join(folderPath, filename));
   }
 
   validate(commandName: string): string | null {
