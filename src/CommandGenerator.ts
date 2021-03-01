37c6baeda9b58c9f5bde4d676eef5b29a1f3d4ac
george.scott <george.scott@sage.com>
2021-03-01 14:42:46 +0000

    separate action and resource entry
    so we can generate namespace correctly

--- src/CommandGenerator.ts
+++ src/CommandGenerator.ts
@@ -16,24 +16,42 @@ export class CommandGenerator { //implements IDisposable {
 			window.showInformationMessage(`Please open an SBC workspace at the route folder`);
       return;
     }
-    
-    let commandName : string | undefined = await this.showInputBox();
 
-    if (!commandName) {
-      window.showInformationMessage(`Please enter a name for the command`);
+    let resourceName : string | undefined = await this.showResourceInputBox();
+    
+    if (!resourceName) {
+      window.showInformationMessage(`Please enter the name of the resource the command will act on`);
       return;
     }
-    commandName = commandName.endsWith('Command') ? commandName : commandName += 'Command';
+    
+    let actionName : string | undefined = await this.showInputBox();
+
+    if (!actionName) {
+      window.showInformationMessage(`Please enter a name for the action the command will perform on the resource`);
+      return;
+    }
+
+    let commandName =  `${actionName}${resourceName}Command`;
 
     await this.fileCreator.createFiles(commandName, workspaceDetail);
     window.showInformationMessage(`Command: '${commandName}' successfully created`);
   }
 
+  async showResourceInputBox(): Promise<string | undefined> {
+    const result = await window.showInputBox({
+      value: 'User',
+      valueSelection: [0, 7],
+      placeHolder: 'For example: User',
+      validateInput: this.validate,
+    });
+    return result;
+  }
+
   async showInputBox(): Promise<string | undefined> {
     const result = await window.showInputBox({
-      value: 'ExampleCommand',
+      value: 'Create',
       valueSelection: [0, 7],
-      placeHolder: 'For example: GetUserCommand',
+      placeHolder: 'For example: Create',
       validateInput: this.validate,
     });
     return result;
